fbb6bbf306caa46b7be9456be7fd8bcb7a8497d3
Jeppe Andersen <jeppe@outlook.com>
2026-02-08 07:12:50 +0100

    Open Apps in New Tab

--- web/app.js
+++ web/app.js
@@ -228,7 +228,12 @@ function launchSelectedApp() {
 }
 
 function launchApp(url) {
-    window.open(url, '_blank', 'noopener,noreferrer');
+    const link = document.createElement('a');
+    link.href = url;
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+    link.referrerPolicy = 'no-referrer';
+    link.click();
     closeCommandPalette();
 }
 
